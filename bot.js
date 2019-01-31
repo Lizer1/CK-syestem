3c0c82cc8cd28d01b37174e228b457b0c62175df
Lizer1 <46378036+Lizer1@users.noreply.github.com>
2019-01-31 13:06:28 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -55,17 +55,7 @@ client.on("reachLimit", (limit)=> {
     .catch(log.send)
   });
   
-});
-client.on('ready', () => {
-   console.log(`----------------`);
-      console.log(`Desert Bot- Script By : i1Suhaib`);
-        console.log(`----------------`);
-      console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
-    console.log(`----------------`);
-  console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`CO Security BY LiZer_PRo`,"http://twitch.tv/S-F")
-client.user.setStatus("dnd")
-});
+
 
 client.login(process.env.BOT_TOKEN);
 
